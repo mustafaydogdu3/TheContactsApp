f3f8db02a274a6f26d0c375a53905a78f3c3effa
= <=>
2024-12-19 17:47:04 +0300

    Display contact page created

--- app/src/main/java/com/mustafa/thecontactsapp/MainActivity.kt
+++ app/src/main/java/com/mustafa/thecontactsapp/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,11 +26,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -41,6 +51,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -55,15 +66,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import coil.compose.rememberAsyncImagePainter
+import coil.imageLoader
 import com.mustafa.thecontactsapp.ui.theme.GreenJC
 import com.mustafa.thecontactsapp.ui.theme.TheContactsAppTheme
 import java.io.File
 import java.io.FileOutputStream
-import java.lang.Exception
 import kotlin.Exception
 
 class MainActivity : ComponentActivity() {
@@ -77,7 +91,98 @@ class MainActivity : ComponentActivity() {
         val viewModel: ContactViewModel by viewModels { ContactViewModelFactory(repository) }
 
         setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "contactList"){
+                composable("contacList"){ContactListScreen(viewModel, navController)}
+                composable("addContact") { AddContactScreen(viewModel, navController)}
+            }
 
+        }
+    }
+
+    @Composable
+    fun ContactItem(contact: Contact, onClick:() -> Unit){
+        Card(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 12.dp, end = 12.dp, top = 8.dp)
+            .clickable(onClick = onClick),
+            colors = CardDefaults.cardColors(Color.White),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .clickable(onClick = onClick)
+                .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(painter = rememberAsyncImagePainter(contact.image),
+                    contentDescription = contact.name,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop)
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = contact.name)
+            }
+
+        }
+
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun ContactListScreen(viewModel: ContactViewModel, navController: NavController){
+        val context = LocalContext.current.applicationContext
+
+        Scaffold(
+            topBar = {
+                TopAppBar(modifier = Modifier.height(48.dp), 
+                    title = { 
+                        Box(modifier = Modifier
+                            .fillMaxHeight()
+                            .wrapContentHeight(Alignment.CenterVertically))
+                        Text(text = "Contacts", fontSize = 10.sp)
+                    }, 
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            Toast.makeText(
+                                context,
+                                "Contacts",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.contact_icon), contentDescription = null)
+                        }
+
+                    }, colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = GreenJC,
+                        titleContentColor = Color.White,
+                        navigationIconContentColor = Color.White
+
+                    )
+                )
+
+            },
+            floatingActionButton = {
+                FloatingActionButton(containerColor = GreenJC,onClick = {navController.navigate("addContact")}) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add Contact")
+
+                }
+            }
+
+
+        ) {paddingValues ->
+            val contacts by viewModel.allContacts.observeAsState(initial = emptyList())
+            LazyColumn(modifier = Modifier.padding(paddingValues)
+            ){
+                items(contacts){contact->
+                    ContactItem(contact = contact) {
+                        navController.navigate("contactDetail/${contact.id}")
+
+                    }
+
+                }
+            }
         }
     }
 
@@ -220,7 +325,7 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    fun copyUriToInternalStroge(context: Context, uri: Uri,fileName:String):String?{
+    fun copyUriToInternalStroge(context: Context, uri: Uri, fileName:String):String?{
         val file = File(context.filesDir,fileName)
         return try{
             context.contentResolver.openInputStream(uri)?.use { inputStream->
